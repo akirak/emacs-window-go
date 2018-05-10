4894f63b6b17bd6ba47f1c12d8c5131f8fa600e3
Akira Komamura <akira.komamura@gmail.com>
2018-05-11 00:43:53 +0900

    Add a library header

--- window-go.el
+++ window-go.el
@@ -1,5 +1,36 @@
 ;;; window-go.el --- A collection of window operation utilities -*- lexical-binding: t -*-
 
+;; Copyright (C) 2018 by Akira Komamura
+
+;; Author: Akira Komamura <akira.komamura@gmail.com>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "25.1") (dash "2.12"))
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This library provides miscellaneous functions for window manipulation.
+;; It tries to emulate behaviors that are somewhat similar to those of XMonad,
+;; but it does not aim at perfection.
+
+;;; Code:
+
 (require 'dash)
 
 (defun window-go-bottom ()
